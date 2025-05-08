a8f3dedc83198c291233b91447e5088ac047ad85
osama <osama@OSSI>
2025-05-08 16:33:48 +0200

    Add project files.

---
+++ TestSkolKompis/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using TestSkolKompis.Core;
+
+namespace TestSkolKompis.MVVM.ViewModel
+{
+
+    internal class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand MinProfilViewCommand { get; set; }
+        public HomeViewModel HomeVm { get; set; }
+
+        public MinProfilViewModel MinProfilVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            HomeVm = new HomeViewModel();
+            MinProfilVM = new MinProfilViewModel();
+            CurrentView = HomeVm;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVm;
+            });
+
+            MinProfilViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = MinProfilVM;
+            });
+        }
+    }
+}
